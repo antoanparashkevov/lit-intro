a1fc9e98ae9dd1f52b211642b34d29eb0cd0ee01
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-02 16:35:02 +0300

    Rendering Articles via lit-html from data.js

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
+import {data} from './data.js'
 
+const template = (name) => html`<h2>Hello there, ${name}</h2>`
 
-const template = () => html`<h2>Hello there</h2>`
+const templateArticle = (article)=>
+    html`<article>
+    <h1>${article.title}</h1>
+    <div class="content">
+        <p>${article.content}
+        </p>
+    </div>
+    <footer>Author: ${article.author}</footer>
+</article>
+`
+
 
 
 start()
@@ -9,6 +21,8 @@ start()
 function start() {
     const main = document.querySelector('main')
 
-    const templateResult = template()
-    render(templateResult, main)
+    // const templateResult = template('Peter')
+    // render(templateResult, main)
+
+    render(data.map(templateArticle), main)//render works as replaceChild(), not as appendChild()
 }
---
+++ data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    "title": "First Article",
+    "content": "Simple first content",
+    "author": "John Smith"
+  },
+  {
+    "title": "Second Article",
+    "content": "Simple second content",
+    "author": "Mary Malone"
+  },
+  {
+    "title": "Third Article",
+    "content": "Simple third content",
+    "author": "Peter Johnson"
+  }
+]
+export const nav = [
+  {
+    href:'/',
+    label:'Home'
+  },
+  {
+    href:'/catalog',
+    label:'Catalog'
+  },
+  {
+    href:'/about',
+    label:'About'
+  },
+
+]
