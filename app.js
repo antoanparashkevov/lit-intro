fd82945f36664b67f596c07b8a3c1fc7bd2c2a00
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-02 16:44:47 +0300

    Create Btn for rendering text for demo purposes

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
 import {data} from './data.js'
 
-const template = (name) => html`<h2>Hello there, ${name}</h2>`
+const template = (name,counter) => html`<h2>Hello there, ${name}. Clicked ${counter}</h2>`
 
-const templateArticle = (article)=>
-    html`<article>
-    <h1>${article.title}</h1>
-    <div class="content">
-        <p>${article.content}
-        </p>
-    </div>
-    <footer>Author: ${article.author}</footer>
-</article>
-`
-
+const templateArticle = (article) =>
+    html`
+        <article>
+            <h1>${article.title}</h1>
+            <div class="content">
+                <p>${article.content}
+                </p>
+            </div>
+            <footer>Author: ${article.author}</footer>
+        </article>
+    `
 
 
 start()
 
 function start() {
+    document.getElementById('reloadBtn').addEventListener('click', onClick)
     const main = document.querySelector('main')
 
-    // const templateResult = template('Peter')
-    // render(templateResult, main)
 
     render(data.map(templateArticle), main)//render works as replaceChild(), not as appendChild()
+}
+let counter = 0;
+function onClick(){
+    const header = document.querySelector('header')
+    const templateResult = template('Peter',++counter)
+    render(templateResult, header)
+
+
+
 }
